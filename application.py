d98bd0cb1115b82cf4f9b92eb5e24990c43fcd72
marina-fares <marina.fares11@gmail.com>
2020-05-13 15:31:17 +0200

    commit signin and signup functions

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import requests
 import numpy as np
 import json
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from models import *
 
@@ -12,24 +12,76 @@ from db_fun import *
 #init application
 app = Flask(__name__)
 socketio=SocketIO(app)
+app.secret_key='this is my own secret'
 
 #init database
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///chatdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+
 @app.route("/")
 def signin():
-    return render_template("signin.html")
+    '''
+    this function for NavBar
+    return : signin page
+    '''
+    return render_template('signin.html')
+
 
 @app.route("/signup")
 def signup():
-    return render_template("signup.html")
-'''
-@app.route("/star")
-def start():
-    return render_template("start.html")
-'''
+    '''
+    this function for NavBar
+    return : signup page
+    '''
+    return render_template('signup.html')
+
+@app.route("/welcome",methods=['POST'])
+def welcome():
+    '''
+    this function return user page  
+    input : user data "username and password"
+    '''
+    rooms=[]
+    rooms= Room.query.add_columns('name').all()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = UserData.query.filter_by(name=username , password=password).first()
+        if not user:
+            flash("username or password not correct please try again")        
+            return render_template('signin.html')
+
+    return render_template("start.html", username = username, rooms = rooms)
+
+@app.route("/storedata", methods=['POST'])
+def storedate():
+    '''
+    this finction when user signup
+    input : user data
+    return : user page
+    '''
+    rooms=[]
+    rooms= Room.query.add_columns('name').all()
+    
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    try:
+        user = UserData(name=username, email=email, password=password )
+        db.session.add(user)
+        db.session.commit()
+        
+        return render_template('start.html', username=username, rooms = rooms )
+    except:
+        flash('this user name is used plaese use another one')
+        return render_template("signin.html")
+
+
+
 @app.route("/viewroom/<username>/<roomname>")
 def ViewRoom(username,roomname):
     members=get_room_members(roomname)
@@ -38,24 +90,6 @@ def ViewRoom(username,roomname):
         add_room_member(roomname, username)
     list_of_messages = get_messages(roomname)
     return render_template("chat.html",roomname=roomname,room=room, members=members, username=username, list_of_messages=list_of_messages )
-
-@app.route('/start',methods=["POST"])
-def storedata():
-    rooms=[]
-    rooms= Room.query.add_columns('name').all()
-    
-    username = request.form.get('username')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    
-    if email is None:
-        user=UserData.query.filter_by(name=username , password=password).first()
-        if not user:
-            return "this user name isn't exist"
-    else:
-        store_user_data(username,email,password)
-        print("Done")
-    return render_template('start.html',username=username, rooms=rooms)
 
 @app.route('/chat',methods=["POST"])
 def chat():
@@ -117,5 +151,6 @@ def handle_send_message_event(data):
 
     app.logger.info("{} has sent message to the room {}: {}".format(data['username'],data['roomname'],data['message']))
     socketio.emit('receive_message', data, room=data['roomname'])
+
 
 socketio.run(app,debug=True)
